68f4fa8de84dcf2a650a09e1f98e97a960d51558
paul-emmanuel.dos-sa <GitHub1418$>
2020-07-27 13:30:58 +0200

    before pull

--- user-api/src/test/java/com/pedsf/library/userapi/service/UserServiceImplTest.java
+++ user-api/src/test/java/com/pedsf/library/userapi/service/UserServiceImplTest.java
@@ -1,19 +1,59 @@
 package com.pedsf.library.userapi.service;
 
+import com.pedsf.library.userapi.repository.RoleRepository;
+import com.pedsf.library.userapi.repository.UserRepository;
 import org.junit.jupiter.api.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 class UserServiceImplTest {
-   private UserService userService;
+
+   @MockBean
+   private static UserRepository userRepository;
+   @MockBean
+   private static RoleRepository roleRepository;
+
+   @Autowired
+   private static UserServiceImpl userService;
+
+   @TestConfiguration
+   static class UserServiceImplTestContextConfiguration {
+
+      @Bean
+      public UserService userService() {
+         return new UserServiceImpl(userRepository,roleRepository);
+      }
+   }
+
+
+   @BeforeAll
+   private static void beforeAll() {
+
+   }
 
    @Test
    @Tag("userDTOtoUser")
    @DisplayName("userDTOtoUser")
    void userDTOtoUser() {
+      Integer nb = userService.count();
+
+      assertThat(nb).isEqualTo(0);
    }
 
    @Test
@@ -104,6 +144,8 @@ class UserServiceImplTest {
    @Tag("count")
    @DisplayName("count")
    void count() {
-//      assertThat(userService.count()).isEqualTo(5);
+      Integer nb = userService.count();
+
+      assertThat(nb).isEqualTo(0);
    }
 }
